79ee1730c4f23258bed9e5f6402fd334b3ea94ae
DadBot9000 <cryptosensor@gmail.com>
2026-03-13 19:03:07 +0000

    Update Toast.js

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import { Animated, Text, View } from 'react-native';
+import { styles } from '../styles/styles';
 
+export default function Toast({ anim, msg, type }) {
+  const translateY = anim.interpolate({ inputRange: [0, 1], outputRange: [-10, 0] });
+  return (
+    <View style={styles.toastWrapper}>
+      <Animated.View
+        style={[
+          styles.toast,
+          type === 'success' && styles.toastSuccess,
+          type === 'error' && styles.toastError,
+          {
+            opacity: anim,
+            transform: [{ translateY }]
+          }
+        ]}
+      >
+        <Text style={styles.toastText}>{msg}</Text>
+      </Animated.View>
+    </View>
+  );
+}
